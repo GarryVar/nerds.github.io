64c0e9fb8120440fe75c89dbcb8543c065a4eb98
GarryVar <darisvet.sokolov@yandex.ru>
2020-02-27 16:19:57 +0300

    Рефакторит script.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,32 +1,34 @@
 (() => {
-  const _DOC = document;
-  let sliderList = _DOC.getElementById("slider"),
-      sliderToggle = _DOC.querySelectorAll(".slider__toggle-dot"),
-      sliderWitdth = [0, -100, -200,],
-      left = 0;
-      console.log(left);
+  const _DOC = document, left = 0;
 
+  function sliderPos() {
+    const sliderWidth = [0, -100, -200];
+    return sliderWidth;
+  }
 
-      function sliderFunction(a) {
-        return () => {
-          sliderList.style.left = `${a}%`;
-        }
-      }
+  function slider() {
+    const slider = _DOC.getElementById('slider');
+    return slider;
+  };
 
-      function sliderFunctionAuto() {
-        left = left - 100;
-        if(left <= -300) {
-          left = 0;
-        }
+  function sliderFunction(list, width) {
+    list().style.left = `${width}%`;
+  };
 
-        sliderList.style.left = `${left}%`;
-      };
-      setInterval(() => {
-        sliderFunctionAuto();
-      },4000);
+  window.addEventListener('click', e => {
+    const elementTarget = e.target;
 
+    if (elementTarget.dataset.toggle === 'first') {
+        e.preventDefault();
+        sliderFunction(slider, sliderPos()[0]);
 
-      for(let i = 0; i < sliderToggle.length; i++) {
-        sliderToggle[i].addEventListener("click", sliderFunction(sliderWitdth[i]));
-      }
+    } else if (elementTarget.dataset.toggle === 'second') {
+        e.preventDefault();
+        sliderFunction(slider, sliderPos()[1]);
+
+    } else if (elementTarget.dataset.toggle === 'third') {
+        e.preventDefault();
+        sliderFunction(slider, sliderPos()[2]);
+    }
+  });
 })();
